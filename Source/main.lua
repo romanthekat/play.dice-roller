a39c8e89007187bd77505014837534fb1275a244
Roman Garanin <git@romangaranin.net>
2024-03-13 20:53:32 +0100

    feat: performance bump by removing cycle updates

--- Source/main.lua
+++ Source/main.lua
@@ -3,23 +3,22 @@ import "CoreLibs/graphics"
 import 'text'
 
 ---- initialisation
-local text = {}
-local diceIndex = 1
+local dices = {2, 4, 6, 8, 10, 12, 20, 100}
+local diceIndex = 3
 
 local newRoll = false
-local rollCount = 0
 local roll = 0
 local prevRoll = 0
-local prevRollsLine = ""
+local rollCount = 0
 local total = 0
 
+local prevRollsLine = ""
+local text = {}
 -- view
 -- playdate.display.setRefreshRate(30)
 
 gfx = playdate.graphics
 font = gfx.font.new('fonts/Roobert/Roobert-20-Medium-table-32-32.png')   
-local needRefresh = true
-local crankStepPerLine = 7
 
 -- code
 gfx.setColor(gfx.kColorWhite)
@@ -28,7 +27,6 @@ gfx.setFont(font)
 math.randomseed(playdate.getSecondsSinceEpoch())
 ---- initialisation finished
 
-local dices = {2, 4, 6, 8, 10, 12, 20, 100}
 function prepareDiceLine(diceIndex)
    local result = ""
    
@@ -59,33 +57,28 @@ end
 function playdate.update() 
    -- playdate.drawFPS(385, 0) 
 
-   if needRefresh then
-      if newRoll and prevRoll > 0 then
-         prevRollsLine = prevRoll .. " " .. prevRollsLine --todo: truncate excess line
-         newRoll = false
-      end
-      
-      text[1] = prepareDiceLine(diceIndex)
-      text[2] = "roll(x" .. rollCount .. "): [" .. roll .. "] " .. prevRollsLine
-      text[3] = "sum: " .. total
-      text[4] = "two max: " .. math.max(roll, prevRoll)
-      text[5] = "two min: " .. math.min(roll, prevRoll)
-      text[6] = "two sum: " .. roll + prevRoll
-      text[7] = "two sub: " .. prevRoll - roll
-      
-      drawText(text, 1)
-      needRefresh = false
+   if newRoll and prevRoll > 0 then
+      prevRollsLine = prevRoll .. " " .. prevRollsLine --todo: truncate excess line
+      newRoll = false
    end
+      
+   text[1] = prepareDiceLine(diceIndex)
+   text[2] = "roll(x" .. rollCount .. "): [" .. roll .. "] " .. prevRollsLine
+   text[3] = "sum: " .. total
+   text[4] = "two max: " .. math.max(roll, prevRoll)
+   text[5] = "two min: " .. math.min(roll, prevRoll)
+   text[6] = "two sum: " .. roll + prevRoll
+   text[7] = "two sub: " .. prevRoll - roll
+      
+   drawText(text, 1)
    
    playdate.stop()
    playdate.display.flush()
-
     -- gfx.sprite.update()
     -- playdate.timer.updateTimers()
 end
 
 function playdate.leftButtonDown()
-   needRefresh = true
    diceIndex -= 1
    if diceIndex <= 0 then
        diceIndex = 1
@@ -96,7 +89,6 @@ end
 
 
 function playdate.rightButtonDown()
-   needRefresh = true
    diceIndex += 1
    if diceIndex > #dices then
        diceIndex = #dices
@@ -107,7 +99,6 @@ end
 
 function playdate.AButtonDown()
    newRoll = true
-   needRefresh = true
    
    rollCount += 1
    prevRoll = roll
@@ -119,8 +110,6 @@ function playdate.AButtonDown()
 end
 
 function playdate.BButtonDown()
-   needRefresh = true
-   
    rollCount = 0
    roll = 0
    prevRoll = 0
