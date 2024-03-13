bf4015be67a992a7b3fa40fdf23426b0669f7f97
Roman Garanin <git@romangaranin.net>
2024-03-13 22:07:31 +0100

    feat: smaller screen usage

--- Source/main.lua
+++ Source/main.lua
@@ -68,9 +68,9 @@ function playdate.update()
    text[1] = prepareDiceLine(diceIndex)
    text[2] = "rolls(x" .. rollCount .. "): [" .. roll .. "] " .. prevRollsLine
    text[3] = "sum: " .. total
-   text[4] = "two max: " .. math.max(roll, prevRoll)
+   text[4] = "pair max: " .. math.max(roll, prevRoll)
    text[5] = "       min: " .. math.min(roll, prevRoll)
-   text[6] = "two sum: " .. roll + prevRoll
+   text[6] = "pair sum: " .. roll + prevRoll
    text[7] = "       sub: " .. prevRoll - roll
       
    drawText(text, 1)
